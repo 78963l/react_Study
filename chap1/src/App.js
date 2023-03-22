b6d1af1023097335fe4773b1b75a85b46774a873
SungMin.kim <78963l@naver.com>
2023-03-22 18:41:26 +0900

    데이터바인딩

--- chap1/src/App.js
+++ chap1/src/App.js
@@ -8,13 +8,19 @@ function App() {
     return 100
   }
 
+  let setColor = { color : 'red', fontSize : '30px'}
+
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
-      <ht4>{ posts }</ht4>
-      <ht4>{ 함수() }</ht4>
+      <ht4>{ posts }</ht4>          {/* 변수 */}
+      <ht4>{ 함수() }</ht4>         {/* 함수 */}
+      <img src={ logo } />          {/* 이미지 */}
+      <div className={posts}></div> {/* 클래스명 */}
+      <div style={{color : 'blue', fontSize : '15px'}}>색상</div>
+      <div style={setColor}>색상</div>
     </div>
   );
 }
