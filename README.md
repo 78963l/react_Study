b743b538299dc3901afb295b613e337a3b01c4ec
SungMin.kim <78963l@naver.com>
2023-03-22 18:25:44 +0900

    test

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # react_Study
+
+# 설치
+1. nodejs install
+2. npx create-react-app chap1
+
+# 실행
+npm start
+
---
+++ chap1/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  let posts = "강남 고기 맛집";
+
+  return (
+    <div className="App">
+      <div className="black-nav">
+        <div>개발 Blog</div>
+      </div>
+      <ht4>{ posts }</ht4>
+    </div>
+  );
+}
+
+export default App;
